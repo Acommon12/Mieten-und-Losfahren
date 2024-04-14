0e0afd1ba6878a0d8e92ec1b88b724aee34b34f6
Acommon12 <165499411+Acommon12@users.noreply.github.com>
2024-04-14 20:56:45 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '127.0.0.1'; // Änderung: Lausche nur auf der lokalen Loopback-Schnittstelle
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'index.html'); // Pfad zur index.html-Datei
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500);
+      res.end(`Server Error: ${err.code}`);
+      return;
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(content, 'utf-8');
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
