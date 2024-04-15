3d1021cfa7ada757f846547b2bb132f05c0beb1e
Acommon12 <165499411+Acommon12@users.noreply.github.com>
2024-04-15 02:25:42 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'index.html'); // Pfad zur index.html-Datei
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500);
+      res.end(`Server Error: ${err.code}`);
+      return;
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(content, 'utf-8');
+  });
+});
+
+const PORT = process.env.PORT || 6969; // Verwende den von Heroku bereitgestellten Port oder Port 6969 als Standardwert
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
